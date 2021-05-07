2df3a3e546ff585271dca83c59f79f926aa62d1f
Andrei <augustinov.andrei@gmail.com>
2021-05-07 21:51:39 +0200

    Spring Initializr

---
+++ src/main/java/com/andreicoctails/AndreiCoctails/AndreiCoctailsApplication.java
@@ -0,0 +1,13 @@
+package com.andreicoctails.AndreiCoctails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndreiCoctailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndreiCoctailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreicoctails/AndreiCoctails/AndreiCoctailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreicoctails.AndreiCoctails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndreiCoctailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
